8065975f9303dad0fc92608499ab7463fa02a19e
Yash Johri <60975996+yashjohri@users.noreply.github.com>
2020-05-13 23:15:21 +0530

    Update script.js

--- module-4-solution/script.js
+++ module-4-solution/script.js
@@ -1,16 +1,17 @@
-{
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function () {
+	
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-    for (var i=0; i<names.length; i++) {
+	for (var i = 0; i < names.length; i++) {
 
-        var firstLetter = names[i].charAt(0);
+		var firstLetter = names[i].charAt(0).toLowerCase();
 
-        if (firstLetter.toLowerCase() == 'j') {
-            byeSpeaker.speak(names[i]);
-        } 
-        else {
-            helloSpeaker.speak(names[i]);
-        }
-    }
+		if (firstLetter === 'j') {
+			byeSpeaker.speak(names[i]);
+		} else {
+			helloSpeaker.speak(names[i]);
+		}
 
-}(window);
+	}
+
+})();
